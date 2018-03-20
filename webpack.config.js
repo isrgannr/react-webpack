c33657b2f57e1f26bf10bbd5324cc7ecc70ef31b
Mert Köseoğlu <info@mkoseoglu.com>
2018-03-20 20:28:39 +0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// webpack.config.js
+
+var path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/dist/"
+  },
+  // add the babel-loader and presets
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: "babel-loader",
+          options: { presets: ['react-app']}
+        }]
+      }
+    ]
+  }
+  // end of babel-loader
+}
